d23193bacf0073ac6c517d218a269adc7c7ea5c5
youssef <youssef.bsf@gmail.com>
2023-10-24 01:24:36 +0400

    update dist

--- dist/learning-header/AuthenticatedUserDropdown.js
+++ dist/learning-header/AuthenticatedUserDropdown.js
@@ -13,10 +13,10 @@ var AuthenticatedUserDropdown = function AuthenticatedUserDropdown(_ref) {
     href: "".concat(getConfig().LMS_BASE_URL, "/dashboard")
   }, intl.formatMessage(messages.dashboard));
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("a", {
-    className: "text-gray-700 mr-3",
+    className: "text-gray-700",
     href: "".concat(getConfig().SUPPORT_URL)
   }, intl.formatMessage(messages.help)), /*#__PURE__*/React.createElement(Dropdown, {
-    className: "user-dropdown"
+    className: "user-dropdown ml-3"
   }, /*#__PURE__*/React.createElement(Dropdown.Toggle, {
     variant: "outline-primary"
   }, /*#__PURE__*/React.createElement(FontAwesomeIcon, {
